50a7b0dbad3457a7523867a59c19c4b4f31059a0
Max Topan <maxtopan@hotmail.co.uk>
2025-02-02 12:32:10 +0000

    add GithubUser viewmodel

---
+++ GithubUsersSearcher/ViewModels/GithubUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+
+namespace GithubUsersSearcher.ViewModels
+{
+    public class GithubUserViewModel
+    {
+        public string Username { get; set; }
+    }
+}
