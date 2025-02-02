1262d3a844cd0b9ab70dd30e59ad62ff2bfbc7a6
Max Topan <maxtopan@hotmail.co.uk>
2025-02-02 16:20:32 +0000

    modify IGithubService

--- GithubUsersSearcher/Services/IGithubService.cs
+++ GithubUsersSearcher/Services/IGithubService.cs
@@ -1,13 +1,9 @@
 ﻿using GithubUsersSearcher.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GithubUsersSearcher.Services
 {
-    internal interface IGithubService
+    public interface IGithubService
     {
         Task<GithubUser> GetUserAsync(string username);
     }
